10576a32acb924abfad9de3a2773a45ba04f9736
todi <todiadiyatmo@tonjoo.com>
2013-09-19 02:13:28 +0700

    add ftp

--- README.md
+++ README.md
@@ -1,13 +1,15 @@
 ##[Tonjoo](http://tonjoo.com/backup-rotation-script/ "Tonjoo") - Bash Backup Rotation Script
 
 ###Description
-This script allow you to create a rotation backup for your files and mysql database.  
+This script allow you to create a rotation backup for your files and mysql database. The backup target can be local or remote (FTP). 
 
-The backup will be split into 3 type of backup :
+The script have 3 `backup type`
 
 - Daily backup
 - Weekly backup (default : perform every sixth day of the week)
 - Monthly backup (default : perform every first day of the month)
+
+This script provide you customize the where to backup of each `backup type`. For example you can choose to do daily and weekly backup on local machine and monthly backup on FTP server.
 
 Each type of backup will have a backup retention(copy). The default retention for every type of backup can be configured. 
 
@@ -17,11 +19,21 @@ The backup retention default day is :
 *   Weekly backup : 14 Day ( ~2 weekly backup )
 *   Monthly backup : 30 Day ( ~1 Monthly backup )
 
-Please consider your space when creating many retention
+Please consider your space when creating many retention.
 
-##Usage Instruction
+###Table of Content
+1. [Usage Instruction](#usage_instruction)
+2. [Backup Type Configuration](#backup_type_configuration)
+3. [FTP Backup Configuration](#ftp_backup_configuration)
+4. [Contribute and Contact](#contribute_and_contact)
+
+<div id='usage_instruction'>
+##1. Usage Instruction
 
 1. Download the `backup_rotation.sh` script into your NIX server.
+
+		wget https://raw.github.com/todiadiyatmo/bash-backup-rotation-script/master/backup_rotation.sh
+
 2. Open the file using your favourite editor (ex :`nano` or `vi`)
 
 		nano backup_rotation.sh
@@ -30,43 +42,31 @@ Please consider your space when creating many retention
 	
 		vi backup_rotation.sh	 
 
-3. Edit the configuration file. MYSQL configuration, `BACKUP\_DIR`, `TARGET\_DIR` must be filled. 
+3. Edit the configuration file. MYSQL configuration, for minimal `BACKUP_DIR`, `TARGET_DIR` must be filled. 
 
-		# Edit this configuration !! 
+		# The directory to be backup , DO NOT END THE DIRECTORY WITH BACKSLASH ! 
+		TARGET_DIR=/target_directory
 		
-		# MYSQL Configuration
-		# Enter all credential within the ' ' single quote !
-		
-		USER='db_user'
-		PASSWORD='db_password'
-		DATABASE='db_database'
-		HOST='127.0.0.1'
-		
-		# The backup directory , do not end the directory with backslash ! 
-		BACKUP_DIR=/your_backup_directory_target
-		
-		# The directory to be backup , do not end the directory with backslash ! 
-		TARGET_DIR=/folder_to_backup
+		# The backup directory , DO NOT END THE DIRECTORY WITH BACKSLASH ! 
+		BACKUP_DIR=/your_backup_direcory
 		
 		# Admin email
 		MAIL="your_email@email.com"
 		
 		# Number of day the daily backup keep
-		RETENTION_DAY=7
+		RETENTION_DAY=3
 		
 		# Number of day the weekly backup keep
-		RETENTION_WEEK=30
+		RETENTION_WEEK=14
 		
 		# Number of day the monthly backup keep
-		RETENTION_MONTH=90
+		RETENTION_MONTH=30
 		
-		#Backup every first day of the month (day of month)
-		MONTH_DAY=1
+		#Monthly date backup option (day of month)
+		MONTHLY_BACKUP_DATE=1
 		
-		#Backup every sixth day of the week (day of week)
-		WEEK_DAY=6
-		
-		# End configuration
+		#Weekly day to backup option (day of week - 1 is monday )
+		WEEKLY_BACKUP_DAY=6
 
 4. Make the script executable
 
@@ -92,6 +92,48 @@ Please consider your space when creating many retention
 
 		yum install cronie
 
-##Contribute and Contact
+<div id='backup_type_configuration'>
+##2. Backup Type Configuration
+
+To customize the `backup type` you must alter the `LOCAL_BACKUP_OPTION` and `FTP_BACKUP_OPTION` in the configuration script.
+
+The configuration value of `LOCAL_BACKUP_OPTION` and `FTP_BACKUP_OPTION` can be view on the table below
+
+![Backup type configuration](http://tonjoo.com/wp-content/uploads/2013/09/tuts.png)
+
+For example, if you want to do daily on local and week on FTP you must fill both options with this value
+
+	LOCAL_BACKUP_OPTION=1
+		
+	FTP_BACKUP_OPTION=2
+
+<div id='ftp_backup_configuration'>
+##3. FTP Backup Configuration
+
+1. Make sure that you fill the `FTP_BACKUP_OPTION` with correct value
+2. Fill the FTP configuration on the script 
+
+		# -----------------
+		# FTP Configuration
+		# Enter all data within the ' ' single quote !
+		# -----------------
+		
+		#This is the FTP servers host or IP address. 
+		FTP_HOST='FTP HOST' 
+		
+		#FTP PORT
+		FTP_PORT=21
+		
+		#This is the FTP user that has access to the server. 
+		FTP_USER='FTP USER'           
+		
+		#This is the password for the FTP user. 
+		FTP_PASSWORD='FTP_PASSWORD'          
+		
+		#The backup directory on remote machine, DO NOT END THE DIRECTORY WITH BACKSLASH ! 
+		FTP_TARGET_DIR='/remote_path'
+
+<div id='contribute_and_contact'>
+##4. Contribute and Contact
 
 Everybody is welcome to fork and contribute. If you need to pull something just post it on the message board. 
--- backup_rotation.sh
+++ backup_rotation.sh
@@ -3,14 +3,23 @@
 #@fork from @Julius Zaromskis(http://nicaw.wordpress.com/2013/04/18/bash-backup-rotation-script/)
 #@description Backup script for your website
 
-
+# --------------------------
 # Edit this configuration !! 
+# --------------------------
 
-# The backup directory , do not end the directory with backslash ! 
-BACKUP_DIR=/your_backup_directory_target
+# -------------------
+# Backup Destination option, please read the README to find the possible value
+# --------------------
 
-# The directory to be backup , do not end the directory with backslash ! 
-TARGET_DIR=/folder_to_backup
+LOCAL_BACKUP_OPTION=7
+
+FTP_BACKUP_OPTION=7
+
+# The directory to be backup , DO NOT END THE DIRECTORY WITH BACKSLASH ! 
+TARGET_DIR=/target_directory
+
+# The backup directory , DO NOT END THE DIRECTORY WITH BACKSLASH ! 
+BACKUP_DIR=/your_backup_direcory
 
 # Admin email
 MAIL="your_email@email.com"
@@ -24,90 +33,139 @@ RETENTION_WEEK=14
 # Number of day the monthly backup keep
 RETENTION_MONTH=30
 
-#Backup every first day of the month (day of month)
-MONTH_DAY=1
+#Monthly date backup option (day of month)
+MONTHLY_BACKUP_DATE=1
 
-#Backup every sixth day of the week (day of week)
-WEEK_DAY=6
+#Weekly day to backup option (day of week - 1 is monday )
+WEEKLY_BACKUP_DAY=6
 
+# -----------------
+# FTP Configuration
+# Enter all data within the ' ' single quote !
+# -----------------
+
+#This is the FTP servers host or IP address. 
+FTP_HOST='FTP HOST' 
+
+#FTP PORT
+FTP_PORT=21
+
+#This is the FTP user that has access to the server. 
+FTP_USER='FTP USER'           
+
+#This is the password for the FTP user. 
+FTP_PASSWORD='FTP_PASSWORD'          
+
+#The backup directory on remote machine, DO NOT END THE DIRECTORY WITH BACKSLASH ! 
+FTP_TARGET_DIR='/remote_path'
+
+# --------------------------------------------------
 # MYSQL Configuration
-# Enter all credential within the ' ' single quote !
+# Enter all data within the ' ' single quote !
+# --------------------------------------------------
 
-USER='db_user'
-PASSWORD='db_password'
-DATABASE='db_database'
-HOST='127.0.0.1'
+DB_USER='DB USER'
+DB_PASSWORD='DB PASSWORD'
+DB_DATABASE='DB DATABASE'
+DB_HOST='127.0.0.1'
 
-
+# -----------------
 # End configuration
+# -----------------
 
 # STARTING BACKUP SCRIPT
 
-
-CURRENT_DIR=${PWD}
-
-mkdir $BACKUP_DIR/backup.incoming
-
-cd $BACKUP_DIR/backup.incoming
-
-# Destination file names
-date_daily=`date +"%d-%m-%Y"`
-
-# Dump MySQL tables
-mysqldump -h $HOST -u $USER -p$PASSWORD $DATABASE > $BACKUP_DIR/backup.incoming/mysql_dump.sql
-
-# Compress tables and files
-tar -czf $BACKUP_DIR/backup.incoming/$date_daily.tgz mysql_dump.sql $TARGET_DIR
-
-# Cleanup
-rm $BACKUP_DIR/backup.incoming/mysql_dump.sql
-
-cd $CURRENT_DIR
-
-# Rotation script
-# Storage folder where to move backup files
-# Must contain backup.monthly backup.weekly backup.daily folders
-# storage=/root/backup/kraton
-# storage = $BACKUP_DIR
-# Source folder where files are backed
-
-#date_weekly=`date +"%V sav. %m-%Y"`
-#date_monthly=`date +"%m-%Y"`
+#Check date
 
 # Get current month and week day number
 month_day=`date +"%d"`
 week_day=`date +"%u"`
 
-# Optional check if source files exist. Email if failed.
-if [ ! -f $BACKUP_DIR/backup.incoming/$date_daily.tgz ]; then
-	mail $MAIL -s "[backup script] Daily backup failed! Please check for missing files."
-fi
-
-# It is logical to run this script daily. We take files from source folder and move them to
-# appropriate destination folder
-
 # On first month day do
-if [ "$month_day" -eq $MONTH_DAY ] ; then
-  	destination=backup.monthly/
-  	# daily - keep for RETENTION_DAY
-	find $BACKUP_DIR/backup.monthly/ -maxdepth 1 -mtime +$RETENTION_DAY -type d -exec rm -rv {} \;
+if [ "$month_day" -eq $MONTHLY_BACKUP_DATE ] ; then
+    BACKUP_TYPE='_monthly'
+    # daily - keep for RETENTION_DAY
+    find $BACKUP_DIR/ -maxdepth 1 -mtime +$RETENTION_DAY -type d -exec rm -rv {} \;
+    COMPARATOR=4
 else
   # On saturdays do
-  if [ "$week_day" -eq $WEEK_DAY ] ; then
-    destination=backup.weekly/
-	# weekly - keep for RETENTION_WEEK
-	find $BACKUP_DIR/backup.weekly/ -maxdepth 1 -mtime +$RETENTION_WEEK -type d -exec rm -rv {} \;
+  BACKUP_TYPE='_weekly'
+  if [ "$week_day" -eq $WEEKLY_BACKUP_DAY ] ; then
+    # weekly - keep for RETENTION_WEEK
+    find $BACKUP_DIR/ -maxdepth 1 -mtime +$RETENTION_WEEK -type d -exec rm -rv {} \;
+  
+    COMPARATOR=2
   else
     # On any regular day do
-    destination=backup.daily/
-  	# monthly - keep for RETENTION_MONTH
-	  find $BACKUP_DIR/backup.daily/ -maxdepth 1 -mtime +$RETENTION_MONTH -type d -exec rm -rv {} \;
+    BACKUP_TYPE='_daily'
+    # monthly - keep for RETENTION_MONTH
+    find $BACKUP_DIR/ -maxdepth 1 -mtime +$RETENTION_MONTH -type d -exec rm -rv {} \;
+   
+    COMPARATOR=1
   fi
 fi
 
-# Move the files
-mkdir $destination
-mv -v $BACKUP_DIR/backup.incoming/* $destination
+PERFORM_LOCAL_BACKUP=0
+PERFORM_FTP_BACKUP=0
+
+# Check wheter to do backup
+if [[ $(( $COMPARATOR & $LOCAL_BACKUP_OPTION )) == $COMPARATOR ]]; then
+  PERFORM_LOCAL_BACKUP=1 
+fi
+
+if [[ $(( $COMPARATOR & $FTP_BACKUP_OPTION )) == $COMPARATOR ]]; then
+  PERFORM_FTP_BACKUP=1 
+fi
 
 
+CURRENT_DIR=${PWD}
 
+mkdir $BACKUP_DIR
+mkdir $BACKUP_DIR/backup.incoming
+
+cd $BACKUP_DIR/backup.incoming
+# Destination file names
+backup_filename=`date +"%d-%m-%Y"`$BACKUP_TYPE
+backup_filename=$backup_filename'.tgz'
+
+# Dump MySQL tables
+mysqldump -h $DB_HOST -u $DB_USER -p$DB_PASSWORD $DB_DATABASE > $BACKUP_DIR/backup.incoming/mysql_dump.sql
+
+# Compress tables and files
+cd $TARGET_DIR
+tar -czf $BACKUP_DIR/backup.incoming/$backup_filename -C $BACKUP_DIR/backup.incoming/ mysql_dump.sql $TARGET_DIR
+
+#clean sql file
+rm $BACKUP_DIR/backup.incoming/mysql_dump.sql
+
+cd $CURRENT_DIR
+
+# Optional check if source files exist. Email if failed.
+if [ ! -f $BACKUP_DIR/backup.incoming/$backup_filename ]; then
+	mail $MAIL -s "[backup script] Daily backup failed! Please check for missing files."
+fi
+
+# FTP
+if [ $PERFORM_FTP_BACKUP -eq 1 ]; then
+ftp -n -v $FTP_HOST $FTP_PORT << END_OF_SESSION
+user $FTP_USER $FTP_PASSWORD
+mkdir $FTP_TARGET_DIR
+cd $FTP_TARGET_DIR
+binary
+put $BACKUP_DIR/backup.incoming/$backup_filename $FTP_TARGET_DIR/$backup_filename
+bye
+END_OF_SESSION
+fi
+
+#Perform local backup
+if [ $PERFORM_LOCAL_BACKUP -eq 1 ]; then
+ 
+  rm -rf $FTP_TARGET_DIR 
+
+  # Move the files
+  mkdir $BACKUP_DIR
+  mv -v $BACKUP_DIR/backup.incoming/* $BACKUP_DIR
+fi
+
+# Cleanup
+rm -rf $BACKUP_DIR/backup.incoming/
